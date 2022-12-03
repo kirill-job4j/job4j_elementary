387ee46783fcab7ab63adfbf2961c2e662e7db72
Indus <mobile.javadriver@gmail.com>
2022-12-04 02:43:42 +0500

    4 после исправления с помощью anydesk

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -20,6 +20,6 @@ public class Converter {
         float expectedE = inE/60;  //не знал как переменную в переод потавить (10/3=3,3333333в переоде)
         float outE = Converter.rubleToDollar(in);
         boolean passedE = expectedE == outE;
-        System.out.println(" 140 rubles are "+ expectedE +" Dollars . Test result :  " + passedE);
+        System.out.println("140 rubles are "+ expectedE +" Dollars . Test result :  " + passedE);
     }
 }
