51f50a442b993a1346afa6474db99764f493bff6
Indus <mobile.javadriver@gmail.com>
2022-12-08 01:20:01 +0500

    4 после исправления с помощью anydesk

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,7 +7,7 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        return value/60;
+        return value / 60;
     }
 
     public static void main(String[] args){
@@ -17,7 +17,7 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 Euro. Test result : " + passed);
         float inE = 140;
-        float expectedE = inE/60;  //не знал как переменную в переод потавить (10/3=3,3333333в переоде)
+        float expectedE = inE / 60;  //не знал как переменную в переод потавить (10/3=3,3333333в переоде)
         float outE = Converter.rubleToDollar(in);
         boolean passedE = expectedE == outE;
         System.out.println("140 rubles are  "+ expectedE +" Dollars . Test result :  " + passedE);
