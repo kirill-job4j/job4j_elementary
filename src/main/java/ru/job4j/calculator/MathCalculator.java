87f95f8fc857e4392917aac530d2ccb38f0b15c4
Indus <mobile.javadriver@gmail.com>
2022-12-08 22:46:53 +0500

    6

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -2,12 +2,12 @@ package ru.job4j.calculator;
 
 public class MathCalculator {
 
-    public static double sumAndMultiply(double first, double second) {
+    public static double sumAndMultiply(double first, double second){
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
     }
 }
--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -6,7 +6,7 @@ public class Converter {
         return value / 70;
     }
 
-    public static float rubleToDollar(float value) {
+    public static float rubleToDollar(float value){
         return value / 60;
     }
 
@@ -20,6 +20,6 @@ public class Converter {
         float expectedE = inE / 60;  //не знал как переменную в переод потавить (10/3=3,3333333в переоде)
         float outE = Converter.rubleToDollar(in);
         boolean passedE = expectedE == outE;
-        System.out.println("140 rubles are "+ expectedE +" Dollars . Test result :  " + passedE);
+        System.out.println("140 rubles are " + expectedE + " Dollars . Test result :  " + passedE);
     }
 }
