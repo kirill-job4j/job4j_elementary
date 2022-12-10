a92d8d119f23cabf87df52871960d19ec3c85ad3
Indus <mobile.javadriver@gmail.com>
2022-12-10 15:02:57 +0500

    13

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,11 +2,11 @@ package ru.job4j.math;
 
 public class MathFunction  {
 
-    public static double sum (double first, double second){
+    public static double sum(double first, double second){
         return first + second;
     }
 
-    public static double multiply (double first, double second){
+    public static double multiply(double first, double second){
         return first * second;
     }
 }
